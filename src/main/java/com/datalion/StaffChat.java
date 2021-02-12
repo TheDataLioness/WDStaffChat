1af77103fac4b2921e47c5b5bca7216a1f8b8e8b
Datalion <damien.odkamp@gmail.com>
2021-02-12 14:08:48 +0100

    Initial Commit

---
+++ src/main/java/com/datalion/StaffChat.java
@@ -0,0 +1,41 @@
+package com.datalion;
+
+import dev.waterdog.event.defaults.PlayerChatEvent;
+import dev.waterdog.logger.Color;
+import dev.waterdog.player.ProxiedPlayer;
+import dev.waterdog.plugin.Plugin;
+import dev.waterdog.utils.Configuration;
+
+public class StaffChat extends Plugin {
+
+    private String staffChatFormat;
+    private char staffChatPrefix;
+
+    @Override
+    public void onEnable() {
+        // Saving and getting config
+        saveResource("config.yml");
+        Configuration cfg = getConfig();
+        staffChatFormat = cfg.getString("staffchat_format");
+        staffChatPrefix = cfg.getString("staffchat_prefix").charAt(0);
+
+        // Register EventManager
+        this.getProxy().getEventManager().subscribe(PlayerChatEvent.class, this::onChat);
+    }
+
+    public void onChat(PlayerChatEvent event){
+        String message = event.getMessage();
+        ProxiedPlayer player = event.getPlayer();
+        if(message.charAt(0) == staffChatPrefix){
+            if(player.hasPermission("staffchat.send")){
+                event.setCancelled();
+
+                String completeMessage = staffChatFormat.replace("{player}", player.getName()).replace("{msg}", message.substring(1)).replace("{server}", player.getServer().getInfo().getServerName());
+                getLogger().info(completeMessage);
+                for (ProxiedPlayer proxiedPlayer : getProxy().getPlayers().values()) {
+                    if(proxiedPlayer.hasPermission("staffchat.receive")) proxiedPlayer.sendMessage(completeMessage);
+                }
+            }
+        }
+    }
+}
